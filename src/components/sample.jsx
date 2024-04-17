4e3eb9212cff688caa957b3821811b8dde854f2b
sathwiksoma <sathwiksoma5@gmail.com>
2024-04-17 09:56:05 +0530

    added sample file

---
+++ src/components/sample.jsx
@@ -0,0 +1,10 @@
+export default function Sample() {
+    return (
+        <>
+        <h1>This is a sample file</h1>
+        <p>It is from Sathwik_branch as first commit</p>
+        </>
+    );
+}
+
+
